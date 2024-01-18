d11676d48e705dd84a1fa5a2e2c75bf0fbde4f0a
hyunsokstar <hyunsokstar@gmail.com>
2024-01-18 14:17:11 +0900

    update

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,10 +1,11 @@
+// src\app\(afterLogin)\layout.tsx
 import { ReactNode } from "react";
 import style from "@/app/(afterLogin)/layout.module.css"
 
 export default function afterLoginLayout({ children }: { children: ReactNode }) {
     return (
         <div className={style.container} >
-            hi
+            {children}
         </div>
     )
 }
