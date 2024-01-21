d943755a1e784cb84cbae639ae7c541e48634004
hyunsokstar <hyunsokstar@gmail.com>
2024-01-21 12:55:28 +0900

    update

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import style from './message.module.css';
+import Room from "@/app/(afterLogin)/messages/_component/Room";
 
-type Props = {}
-
-const Messages = (props: Props) => {
+export default function Home() {
     return (
-        <div>Messages</div>
+        <main className={style.main}>
+            <div className={style.header}>
+                <h3>쪽지</h3>
+            </div>
+            <Room />
+            <Room />
+            <Room />
+            <Room />
+            <Room />
+            <Room />
+        </main>
     )
 }
 
-export default Messages
+// npm install msw --save-dev
